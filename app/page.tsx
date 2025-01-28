9d257e797e34917faf571d128871fbf28b2427af
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-01-28 10:13:57 +0800

    update: add floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { ModeToggle } from "@/components/ModeToggle";
+import { navItems } from "@/data";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 
 export default function Home() {
   return (
-    <div className="flex w-screen flex-col items-center justify-center border border-red-700">
-      <ModeToggle />
+    <div className="flex w-full h-[200vh] flex-col items-center justify-center">
+      <FloatingNav navItems={navItems}/>
     </div>
   );
 }
---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+    { name: "Home", link: "#home" },
+    { name: "About", link: "#about" },
+    { name: "Contact", link: "#contact" },
+];
